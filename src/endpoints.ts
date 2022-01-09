32ce1537a6a6a407ba074f4cb638c178066f5142
fhesselfeld <franziska.hesselfeld@extern.mmi-media.de>
2022-01-09 20:30:08 +0100

    Fixed missing config in endpoints for usermanagement backend

--- src/endpoints.ts
+++ src/endpoints.ts
@@ -2,8 +2,9 @@ export const mealManagerUrl =
   "https://papaya-mealmanager-be-liimootbm.cloud.okteto.net";
 export const paymentServiceUrl =
   "https://papaya-paymentservice-be-liimootbm.cloud.okteto.net";
-export const userManagementUrl = "http://localhost:8080";
+export const userManagementUrl =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080"
+    : "https://papaya-usermanagement-be-liimootbm.cloud.okteto.net";
 export const orderingSystemUrl =
   "https://papaya-orderingsystem-be-liimootbm.cloud.okteto.net";
-//export const userManagementUrl =
-//"https://papaya-usermanagement-be-liimootbm.cloud.okteto.net";
