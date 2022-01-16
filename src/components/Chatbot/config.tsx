5c30c0e0bb7bed4f49204b9350fb82a390aa134e
studlintmo6711 <moritz.lintterer@stud.th-rosenheim.de>
2022-01-16 03:21:09 +0100

    adding basic features for chatbot

---
+++ src/components/Chatbot/config.tsx
@@ -0,0 +1,50 @@
+import { createChatBotMessage } from "react-chatbot-kit";
+import IConfig from "react-chatbot-kit/build/src/interfaces/IConfig";
+import SmartToyIcon from "@mui/icons-material/SmartToy";
+import InitialOptions from "./initialOptions";
+
+const config: IConfig = {
+  initialMessages: [
+    createChatBotMessage(
+      `Hallo, ich bin der Chatbot vom Papaya Lieferdienst!`,
+      { widget: "initialOptions" }
+    ),
+  ],
+  botName: "Papaya Lieferdienst",
+  widgets: [
+    {
+      widgetName: "initialOptions",
+      widgetFunc: (props: any) => <InitialOptions {...props} />,
+      mapStateToProps: ["messages"],
+      props: {},
+    },
+  ],
+  customComponents: {
+    botAvatar: () => (
+      <SmartToyIcon style={{ fill: "black", fontSize: 35, marginRight: 5 }} />
+    ),
+    header: () => (
+      <div
+        style={{
+          alignItems: "center",
+          backgroundColor: "#515151f2",
+          color: "white",
+          borderTopLeftRadius: 5,
+          borderTopRightRadius: 5,
+        }}
+      >
+        Papaya Chatbot
+      </div>
+    ),
+  },
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#2d3134f2",
+    },
+    chatButton: {
+      backgroundColor: "#2d3134f2",
+    },
+  },
+};
+
+export default config;
---
+++ src/components/Chatbot/initialOptions.tsx
@@ -0,0 +1,55 @@
+import { Button } from "@mui/material";
+import React from "react";
+import { createClientMessage } from "react-chatbot-kit";
+import { ActionProvider } from ".";
+
+interface Props {
+  actionProvider: ActionProvider;
+  setState: any;
+}
+
+export default function InitialOptions(props: Props) {
+  const { actionProvider, setState } = props;
+
+  const options = [
+    {
+      text: "Lieferzeit",
+      onClick: () => {
+        const message = createClientMessage("Lieferzeit", {});
+
+        setState((prevState: any) => ({
+          ...prevState,
+          messages: [...prevState.messages, message],
+        }));
+        actionProvider.handleDeliveryTime();
+      },
+    },
+    {
+      text: "Kontakt?",
+      onClick: () => {
+        const message = createClientMessage("Kontakt", {});
+
+        setState((prevState: any) => ({
+          ...prevState,
+          messages: [...prevState.messages, message],
+        }));
+        actionProvider.handleContact();
+      },
+    },
+  ];
+  return (
+    <div>
+      {options.map((option: any, index) => (
+        <Button
+          key={option.text + index}
+          onClick={option.onClick}
+          variant="contained"
+          style={{ margin: "2px" }}
+          sx={{ backgroundColor: "#515151f2" }}
+        >
+          {option.text}
+        </Button>
+      ))}
+    </div>
+  );
+}
